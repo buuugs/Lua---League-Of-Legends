d0083ece4597bd54c8ceba025677bd3884c32b3b
ajgoreq <mcprox123@gmail.com>
2014-12-16 15:31:08 +0100

    Update zedcombo.lua

--- zedcombo.lua
+++ zedcombo.lua
@@ -33,13 +33,16 @@ if autoupdateenabled then
 	AddTickCallback(update)
 end
 
+if myHero.charName ~= "Zed" then return end
+
 function OnLoad()
      ts = TargetSelector(TARGET_LOW_HP_PRIORITY,625)
      Config = scriptConfig("Zed By Ajgoreq Beta", "zedajgoreq")
      Config:addParam("combozed", "Combo", SCRIPT_PARAM_ONKEYDOWN, false, 32)
      Config:addParam("usespellrzed", "Use R to combo", SCRIPT_PARAM_ONOFF, true)
-     Config:addParam("circlezed", "Draw Circle Q", SCRIPT_PARAM_ONKEYTOGGLE, true, string.byte("M"))
+     Config:addParam("circlezed", "Draw Circle", SCRIPT_PARAM_ONKEYTOGGLE, true, string.byte("M"))
 end
+
 
 function OnTick()
      ts:update()
@@ -66,6 +69,5 @@ function OnDraw()
 		  DrawCircle(myHero.x, myHero.y, myHero.z, 550, 0xFF00FF)
 		  DrawCircle(myHero.x, myHero.y, myHero.z, 290, 0xFF00FF)
 		  DrawCircle(myHero.x, myHero.y, myHero.z, 625, 0xFF00FF)
-     end
 end
 end
