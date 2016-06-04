e9ddc27e20e5429eaea8c5f0bbe6fddf61d076bd
ajgoreq <ajgoreq@users.noreply.github.com>
2016-06-04 10:43:59 +0200

    Create CSHacked-SkinChanger.lua

---
+++ CSHacked-SkinChanger.lua
@@ -0,0 +1,26 @@
+--[[
+   _____  _____ _    _            _            _             _____ _    _         _    _            _    
+  / ____|/ ____| |  | |          | |          | |           / ____| |  (_)       | |  | |          | |   
+ | |    | (___ | |__| | __ _  ___| | _____  __| |  ______  | (___ | | ___ _ __   | |__| | __ _  ___| | __
+ | |     \___ \|  __  |/ _` |/ __| |/ / _ \/ _` | |______|  \___ \| |/ / | '_ \  |  __  |/ _` |/ __| |/ /
+ | |____ ____) | |  | | (_| | (__|   <  __/ (_| |           ____) |   <| | | | | | |  | | (_| | (__|   < 
+  \_____|_____/|_|  |_|\__,_|\___|_|\_\___|\__,_|          |_____/|_|\_\_|_| |_| |_|  |_|\__,_|\___|_|\_\
+                          ____                   _  _____       _____       ____                         
+                         |  _ \            /\   (_)/ ____|     |  __ \     / __ \                        
+                         | |_) |_   _     /  \   _| |  __  ___ | |__) |___| |  | |                       
+                         |  _ <| | | |   / /\ \ | | | |_ |/ _ \|  _  // _ \ |  | |                       
+                         | |_) | |_| |  / ____ \| | |__| | (_) | | \ \  __/ |__| |                       
+                         |____/ \__, | /_/    \_\ |\_____|\___/|_|  \_\___|\___\_\                       
+                                 __/ |         _/ |                                                      
+                                |___/         |__/                                                       
+								
+								
+Wersja : 0.01
+Autor : AjGoReQ
+For : CSHacked								
+--]]
+-- Początek szyfrowania
+
+assert(load(Base64Decode("YXNzZXJ0KGxvYWQoQmFzZTY0RGVjb2RlKCJablZ1WTNScGIyNGdUMjVNYjJGa0tDa05DazFsYm5Vb0tRMEtaVzVrRFFwbWRXNWpkR2x2YmlCTlpXNTFLQ2tOQ25CeWFXNTBLQ0k4WWo0OFptOXVkQ0JqYjJ4dmNqMWNJaU0yTmprNVJrWmNJajViUTFOSVlXTnJaV1FnTFNCVGEybHVRMmhoYm1kbGNsMDZJRnBoYkdGa2IzZGhibmtnSVR3dlptOXVkRDRpS1EwS1ExTklJRDBnYzJOeWFYQjBRMjl1Wm1sbktDSkRVMGhoWTJ0bFpDQlRhMmx1SUVOb1lXNW5aWElpTENBaVExTklZV05yWldRZ1UydHBiaUJEYUdGdVoyVnlJaWtOQ2tOVFNEcGhaR1JRWVhKaGJTZ2ljMnRwYmlJc0lDSlhiR0ZqZWk5WGVXeGhZM29pTENCVFExSkpVRlJmVUVGU1FVMWZUMDVQUmtZc0lIUnlkV1VwRFFwRFUwZzZZV1JrVUdGeVlXMG9Jbk5yYVc1c2FYTjBJaXdnSWxOcmFXNGlMQ0JUUTFKSlVGUmZVRUZTUVUxZlRFbFRWQ3dnTVN3Z2V5SXhJQzBnVTJ0cGJpSXNJaklnTFNCVGEybHVJaXdpTXlBdElGTnJhVzRpTENJMElDMGdVMnRwYmlJc0lqVWdMU0JUYTJsdUlpd2lOaUF0SUZOcmFXNGlMQ0kzSUMwZ1UydHBiaUlzSWpnZ0xTQlRhMmx1SWl3aU9TQXRJRk5yYVc0aUxDSXhNQ0F0SUZOcmFXNGlMQ0l4TVNBdElGTnJhVzRpTENJeE1pQXRJRk5yYVc0aUxDSXhNeUF0SUZOcmFXNGlmU2tOQ2tOVFNEcHpaWFJEWVd4c1ltRmpheWdpYzJ0cGJpSXNJR1oxYm1OMGFXOXVJQ2gyWVd4MVpTa05DbWxtSUhaaGJIVmxJSFJvWlc0TkNsTmxkRk5yYVc0b2JYbElaWEp2TENCdFpXNTFMbk5yYVc1c2FYTjBMVEVwRFFwbGJITmxEUXBUWlhSVGEybHVLRzE1U0dWeWJ5d2dMVEVwRFFwbGJtUU5DbVZ1WkNrTkNtMWxiblU2YzJWMFEyRnNiR0poWTJzb0luTnJhVzVzYVhOMElpd2dablZ1WTNScGIyNGdLSFpoYkhWbEtRMEthV1lnYldWdWRTNXphMmx1SUhSb1pXNE5DbE5sZEZOcmFXNG9iWGxJWlhKdkxDQnRaVzUxTG5OcmFXNXNhWE4wTFRFcERRcGxibVFOQ21WdVpDa0pEUXBsYm1RTkNnMEtEUW89IiksIG5pbCwgImJ0IiwgX0VOVikpKCk="), nil, "bt", _ENV))()
+
+-- Koniec szyfrowania
